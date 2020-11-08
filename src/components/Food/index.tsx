27b6798d20ce9ecefe2b2af1ce18803413f0e544
Vinicius Melo <viniciusmelo.dev@gmail.com>
2020-11-08 18:02:32 -0300

    Desafio Concluido

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { FiEdit3, FiTrash } from 'react-icons/fi';
+import api from '../../services/api';
 
 import { Container } from './styles';
 
@@ -28,10 +29,14 @@ const Food: React.FC<IProps> = ({
 
   async function toggleAvailable(): Promise<void> {
     // TODO UPDATE STATUS (available)
+    const data = { ...food, available: !isAvailable };
+    setIsAvailable(!isAvailable);
+    await api.put(`/foods/${food.id}`, data);
   }
 
   function setEditingFood(): void {
     // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
